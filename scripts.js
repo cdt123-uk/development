c8792811797d981e555d5a2d08ed980530499cd1
cdt123-uk <christownend@gmail.com>
2024-08-15 20:05:22 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('nav ul li a').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
+            // handles clicks whilst enabling smooth scrolling.
+            window.location.href = this.getAttribute('href');
 
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
